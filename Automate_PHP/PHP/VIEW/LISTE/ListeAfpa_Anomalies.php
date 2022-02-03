0415c6a52a6bf121d3a3bf4ab0442d59dae2ef69
Billel Berkat <berkat.n@hotmail.com>
2022-02-03 15:24:15 +0100

    ajout graph dans anomalie

--- Automate_PHP/PHP/VIEW/LISTE/ListeAfpa_Anomalies.php
+++ Automate_PHP/PHP/VIEW/LISTE/ListeAfpa_Anomalies.php
@@ -1,83 +1,110 @@
 <?php
 
 
-echo '<div class="bigEspace"></div>';
- echo '<main>';
+	echo '<div class="bigEspace"></div>';
+echo '<main>';
+
  echo '<div class="colonne center">';
+
+
  echo '<div class="bigEspace"></div>';
 
-echo '<div class="Partie1">';
-echo '<div class="grid-col-9">';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div class="center"> Debut :</div>';
-echo '<div></div>';
-echo '<div> <input id="inputR" class="center" type=text id="date"  placeholder="Date"> </div>';
-echo '<div></div>';
-echo '<div> <input id="inputR" type=text class="center" placeholder="Heure"> </div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div class="center"> Fin :</div>';
-echo '<div></div>';
-echo '<div> <input id="inputR" type=text  placeholder="Date"> </div>';
-echo '<div></div>';
-echo '<div> <input id="inputR" type=text  placeholder="Heure"> </div>';
-echo '<div></div>';
-echo '<div> <input class="center" id="valider" type=button Value="Valider"> </div>';
-echo '<div></div>';
-echo '<select> 
-			<option value="Global">Global</option>
-			<option value="Temperatures">Temperatures</option>
-			<option value="Sons">Sons</option>
-			<option value="lumieres">Lumieres</option>
-	  </select>';
-	  echo '<div></div>';
-	  echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '<div></div>';
-echo '</div>';
+
+	echo '<div class="Partie1">';
+
+		echo '<div class="grid-col-9">';
+
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div class="center"> Debut :</div>';
+			echo '<div></div>';
+			echo '<div> <input id="inputR" class="center" type=text id="date"  placeholder="Date"> </div>';
+			echo '<div></div>';
+			echo '<div> <input id="inputR" type=text class="center" placeholder="Heure"> </div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div class="center"> Fin :</div>';
+			echo '<div></div>';
+			echo '<div> <input id="inputR" type=text  placeholder="Date"> </div>';
+			echo '<div></div>';
+			echo '<div> <input id="inputR" type=text  placeholder="Heure"> </div>';
+			echo '<div></div>';
+			echo '<div> <input class="center" id="valider" type=button Value="Valider"> </div>';
+			echo '<div></div>';
+			echo 	'<select> 
+						<option value="Global">Global</option>
+						<option value="Temperatures">Temperatures</option>
+						<option value="Sons">Sons</option>
+						<option value="lumieres">Lumieres</option>
+					</select>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+			echo '<div></div>';
+		echo '</div>';
+
+	echo '</div>';
+
+
+
+	echo '<div class="bigEspace"></div>';
+
+
+
+	echo '<div class="Partie2 center taille colonne">';
+
+		echo '<div class="center graphiqueAnomalie"> Anomalies Statistique Global</div>';
+
+	echo '</div>';
+
+	echo '<div class="bigEspace"></div>';
+
+
+	echo '<img  class="imggraph" src="IMG/graph3.png" alt="">';
+
+
+	echo '<div class="bigEspace"></div>';
+
+
+	echo '<div class="Partie3">';
+
+		echo '<div class="grid-col-3 taille">';
+
+			echo '<div class="grid-columns-span-3 caseListe center"> Liste Anomalies Global </div>';
+			echo '<div class="center caseListe">Type</div>';
+			echo '<div class="center caseListe">Date</div>';
+			echo '<div class="center caseListe">Message d\'erreur</div>';
+			echo '<div class="caseListe">Temperature</div>';
+			echo '<div class="caseListe">2022-01-03</div>';
+			echo '<div class="caseListe">Température ne fonctionne pas </div>';
+			echo '<div class="caseListe">Lumiere</div>';
+			echo '<div class="caseListe">2022-01-03</div>';
+			echo '<div class="caseListe">Lumière ne fonctionne pas</div>';
+			echo '</div>';
+
+		echo '</div>';
+
+
 echo '</div>';
 
-echo '<div class="bigEspace"></div>';
-echo '<div class="Partie2 graphiqueAnomalie center taille">';
 
-echo '<div class=" center"> Anomalies Statistique Global     </div>';
 
-echo '</div>';
-echo '<div class="BIGEspace"></div>';
-// echo '<div class="BIGEspace"></div>';
-
-echo '<div class="Partie3">';
-echo '<div class="grid-col-3 taille">';
-echo '<div class="grid-columns-span-3 caseListe center"> Liste Anomalies Global </div>';
-echo '<div class="center caseListe">Type</div>';
-echo '<div class="center caseListe">Date</div>';
-echo '<div class="center caseListe">Message d\'erreur</div>';
-echo '<div class="caseListe">Temperature</div>';
-echo '<div class="caseListe">2022-01-03</div>';
-echo '<div class="caseListe">Température ne fonctionne pas </div>';
-echo '<div class="caseListe">Lumiere</div>';
-echo '<div class="caseListe">2022-01-03</div>';
-echo '<div class="caseListe">Lumière ne fonctionne pas</div>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
 echo '</main>';
